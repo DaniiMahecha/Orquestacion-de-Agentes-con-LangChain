8487d12a712fab50b383f0641aad485449093b4a
DaniiMahecha <danielfelipemahechapena1010@gmail.com>
2026-07-17 22:30:50 -0500

    LangChain | Prompt Templates

--- 2199-python-y-gemini-orquestando-llms-con-langchain-main/LangChainRecursos/prompt_template.py
+++ 2199-python-y-gemini-orquestando-llms-con-langchain-main/LangChainRecursos/prompt_template.py
@@ -1,6 +1,7 @@
-from langchain.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, PromptTemplate 
 
-template = ChatPromptTemplate.from_messages(
+# TEMPLATE PARA LLM MULTIMODALES
+template_entrada = ChatPromptTemplate.from_messages(
     [
         (
             "system",
@@ -8,6 +9,10 @@ template = ChatPromptTemplate.from_messages(
             Asume que eres analista de imagénes. 
             Tu principal tarea consiste en:
             Analizar una imagen para extraer las informaciones más relevantes de manera objetiva
+
+            # FORMATO DE SALIDA
+            Descripción de la imagen: Tu descripción de la imagen aquí.
+            Etiquetas: Una lista con 3 palabras-clave separadas por comas (,)
             """
         ),
         (   "user",
@@ -25,4 +30,15 @@ template = ChatPromptTemplate.from_messages(
     ]
 )
 
+# TEMPLATE PARA LLM QUE ESPECIALIZADOS EN SOLO TEXTO O MÁS SENCILLOS
+template_salida = PromptTemplate(
+    template = """
+                Genera un resumen, utilizando un lenguaje claro y objetivo,
+                enfocado en el publico colombiano. La idea es que la comunicación del resultado sea
+                lo más sencilla posible, priorizando los registros para consultas posteriores.
 
+                #RESULTADO DE LA IMAGEN
+                {image_analysis}
+                """,
+                input_variables=["image_analysis"]
+)
--- 2199-python-y-gemini-orquestando-llms-con-langchain-main/integraciones/template_in_action.py
+++ 2199-python-y-gemini-orquestando-llms-con-langchain-main/integraciones/template_in_action.py
@@ -1,15 +1,24 @@
-from LangChainRecursos.prompt_template import template
+from LangChainRecursos.prompt_template import template_entrada, template_salida
 from LLMs.LLM_Gemini import llmGemini
+from LLMs.LLM_Cohere import llmCohere
 from langchain_core.output_parsers import StrOutputParser
 from my_helper import encode_image
+from langchain_core.globals import set_debug
+
+set_debug(True)
 
 # Imagen en cadena de texto
 image=encode_image("datos\ejemplo_grafico.jpg")
 
 # LangChain Expression Language (LCEL).
-lcel= template | llmGemini | StrOutputParser
+image_analysis = template_entrada | llmGemini | StrOutputParser()
+sumary = template_salida | llmCohere | StrOutputParser()
+
+result = (image_analysis | sumary)
 
 # Respuesta del LLM
+# StrOutputParser limpia la respuesta del modelo y se encarga de
+# devolver la respuesta en un simple cadena de caracteres.
 response=lcel.invoke({"image":image})
 
 print(response)
